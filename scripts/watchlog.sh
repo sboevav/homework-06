e0e6caaba1f8ced1976fb880e9f430a83ed937c2
Sboev <sboevav@mail.ru>
2019-12-27 15:42:20 +0400

    Доработка ДЗ

--- scripts/watchlog.sh
+++ scripts/watchlog.sh
@@ -13,17 +13,12 @@ ADDR_COUNT=$5
 # Дата/время запуска скрипта
 DATE=`date`
 
-MESSAGE="/tmp/log_checking.txt"
+MESSAGE=/tmp/log_checking.txt
 VARS_FILE=/etc/watchlogvars/vars
 REC_NO=0
+var1=qw
+var2=123
 LAST_DATE="-"
-
-#echo "LOG=$LOG"
-#echo "EMAIL=$EMAIL"
-#echo "PERIOD=$PERIOD"
-#echo "IP_COUNT=$IP_COUNT"
-#echo "ADDR_COUNT=$ADDR_COUNT"
-#echo "DATE=$DATE"
 
 #logger "LOG=$LOG"
 #logger "EMAIL=$EMAIL"
@@ -46,20 +41,20 @@ echo "+------------------------------+" >> $MESSAGE
 }
 
 checkLog () {
-# рабочий вариант - адрес впереди 
-#cat $LOG | awk '/GET/{ ipcount[$1]++ } END { for (i in ipcount) { printf "IP: %15s - %d times\n", i, ipcount[i] } }' | sort -rnk4 | head -$IP_COUNT
-#cat $LOG | awk '/GET/{ addrcount[$11]++ } END { for (i in addrcount) { printf "ADDR: %50s - %d times\n", i, addrcount[i] } }' | sort -rnk4 | head -$ADDR_COUNT
-
   # рабочий вариант - количество впереди
-  echo "$IP_COUNT IP адресов с наибольшим кол-вом запросов" >> $MESSAGE
+  echo "Обработка журнала со времени последнего запуска ($LAST_DATE)"
+
+  echo "$IP_COUNT IP адресов с наибольшим количеством запросов" >> $MESSAGE
   cat $LOG | awk '/GET/{ ipcount[$1]++ } END { for (i in ipcount) { printf "%4d times - IP: %s\n", ipcount[i], i } }' | sort -rnk1 | head -$IP_COUNT >> $MESSAGE
 
-  echo "$ADDR_COUNT запрашиваемых адресов с наибольшим кол-вом запросов" >> $MESSAGE
+  echo "$ADDR_COUNT запрашиваемых адресов с наибольшим количеством запросов" >> $MESSAGE
   cat $LOG | awk '/GET/{ addrcount[$11]++ } END { for (i in addrcount) { printf "%4d times - addr: %50s\n", addrcount[i], i } }' | sort -rnk1 | head -$ADDR_COUNT >> $MESSAGE
 
-  echo "Полный список запросов со времени последнего запуска ($LAST_DATE), с кодом возврата, отличающегося от 200 и 301" >> $MESSAGE
+  echo "Полный список запросов с кодом возврата, отличающегося от 200 и 301" >> $MESSAGE
   cat $LOG | awk '$9 != 200 && $9 != 301' >> $MESSAGE
 
+  echo "Перечень всех кодов возврата с указанием их количества" >> $MESSAGE
+  cat $LOG | awk '{ ipcount[$9]++ } END { for (i in ipcount) { printf "result:%4d - %4d times\n", i, ipcount[i]} }' | sort -nk2 >> $MESSAGE
 }
 
 #TEMPVAR=0
@@ -70,14 +65,21 @@ then
   sudo touch $VARS_FILE
   sudo chmod +777  $VARS_FILE
 else
-  read REC_NO < "$VARS_FILE"
-  logger "REC_NO=$REC_NO"
+  while read REC_NO var2 var3; 
+  do   
+    logger "REC_NO=$REC_NO"
+    logger "var2=$var2"
+    logger "var3=$var3";
+  done < "$VARS_FILE"
+
+#  awk '{ vars[$1]=$1 } END { for (i in vars) { printf "%10s=%s\n", i, vars[i]} }'
+#  read REC_NO < "$VARS_FILE"
 #  read TEMPVAR < "$VAR_SFILE"
 #  logger "TEMPVAR=$TEMPVAR"
 fi
 
 
-#if [ -n "$num" ]; then 
+#if [ -n "$num" ]; then
 #      "переменная что-то имеет и можно запустить другой процесс"
 #else
 #   echo "пустая переменная, останавливаем скрипт"	
@@ -91,15 +93,19 @@ createMessageFile
 logger "Message=$(< $MESSAGE)"
 
 # Пошлем сформированный файл почты
-#sudo bash /vagrant/scripts/sendemail.sh $EMAIL "Log_checking" $MESSAGE
+#mail -s "Log_checking" "$EMAIL" < $MESSAGE
+sudo bash /vagrant/scripts/sendemail.sh $EMAIL "Log_checking ($DATE)" $MESSAGE
 # Удалим исходный файл письма
-rm $MESSAGE
+#rm $MESSAGE
 
 REC_NO=$((REC_NO+1))
 #TEMPVAR=$((TEMPVAR+1000))
 
 # сохраним переменные в файле
 echo "$REC_NO" > $VARS_FILE
+echo "$var2" >> $VARS_FILE
+echo "$var3" >> $VARS_FILE
+
 #echo "$TEMPVAR" >> $VARS_FILE
 
 exit 0
